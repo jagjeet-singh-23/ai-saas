f6c96b1b05243d14456e921637398d5f4f509d86
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-07-22 17:40:49 +0530

    modified:   app/(dashboard)/(routes)/dashboard/page.tsx 	new file:   app/(dashboard)/layout.tsx 	new file:   components/Sidebar.tsx 	new file:   components/navbar.tsx 	new file:   public/logo.png

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const DashBoard = () => {
   return (
     <div>
       <p>DashBoard Page (Protected)</p>
-      <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
