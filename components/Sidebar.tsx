3c3de8b4066cc053d63665635d571c017fdd5a60
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-07-22 19:20:47 +0530

    modified:   components/Sidebar.tsx 	new file:   components/mobile-sidebar.tsx 	modified:   components/navbar.tsx 	new file:   components/ui/sheet.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,8 +12,9 @@ import {
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+
 const montserrat = Montserrat({
   weight: "600",
   subsets: ["latin"],
@@ -64,6 +65,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -80,7 +82,10 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:bg-white/10 transition rounded-lg"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:bg-white/10 transition rounded-lg",
+                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
