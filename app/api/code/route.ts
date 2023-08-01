60dd47a714d64984157df8a14e2df2d6dccfda86
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-01 22:35:20 +0530

    modified:   app/api/code/route.ts 	modified:   app/api/conversation/route.ts 	modified:   app/api/image/route.ts 	modified:   app/api/music/route.ts 	modified:   app/api/video/route.ts 	new file:   lib/api-limit.ts

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,46 +1,52 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
 
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_API_KEY
+  apiKey: process.env.OPEN_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 const instructionMessage: ChatCompletionRequestMessage = {
-    role: "system",
-    content: "You are a code generator. Your task is to generate code according to the query provided to you. You must answer in markdown only, you may use code comments for your code explanation."
-}
+  role: "system",
+  content:
+    "You are a code generator. Your task is to generate code according to the query provided to you. You must answer in markdown only, you may use code comments for your code explanation.",
+};
 
-export async function POST(
-    req: Request,
-) {
-    try {
-        const { userId } = auth();
-        const body = await req.json();
-        const { messages } = body;
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
 
-        if (!userId) {
-            return new NextResponse("Unauthorized Access", { status: 401 });
-        }
-
-        if (!configuration.apiKey) {
-            return new NextResponse("OpenAI Api key not configured", { status: 500 });
-        }
-
-        if (!messages) {
-            return new NextResponse("Messages are required", { status: 400 });
-        }
-
-        const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [instructionMessage, ...messages],
-        });
-
-        return NextResponse.json(response.data.choices[0].message);
-    } catch (error) {
-        console.log("[CODE_ERROR]", error);
-        return new NextResponse("Internal Server Error", { status: 500 });
+    if (!userId) {
+      return new NextResponse("Unauthorized Access", { status: 401 });
     }
+
+    if (!configuration.apiKey) {
+      return new NextResponse("OpenAI Api key not configured", { status: 500 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+    const freeTrail = await checkApiLimit();
+    if (!freeTrail) {
+      return new NextResponse("Free trail is expired", { status: 403 });
+    }
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, ...messages],
+    });
+
+    await increaseApiLimit();
+    
+    return NextResponse.json(response.data.choices[0].message);
+  } catch (error) {
+    console.log("[CODE_ERROR]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,39 +1,42 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
 
 const replicate = new Replicate({
-    auth: process.env.REPLICATE_AI_API_KEY!
-})
+  auth: process.env.REPLICATE_AI_API_KEY!,
+});
 
-export async function POST(
-    req: Request,
-) {
-    try {
-        const { userId } = auth();
-        const body = await req.json();
-        const { prompt } = body;
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
 
-        if (!userId) {
-            return new NextResponse("Unauthorized Access", { status: 401 });
-        }
-
-        if (!prompt) {
-            return new NextResponse("Prompts are required", { status: 400 });
-        }
-
-        const response = await replicate.run(
-            "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
-            {
-                input: {
-                    prompt_a: prompt,
-                }
-            }
-        );
-
-        return NextResponse.json(response);
-    } catch (error) {
-        console.log("[MUSIC_ERROR]", error);
-        return new NextResponse("Internal Server Error", { status: 500 });
+    if (!userId) {
+      return new NextResponse("Unauthorized Access", { status: 401 });
     }
+
+    if (!prompt) {
+      return new NextResponse("Prompts are required", { status: 400 });
+    }
+    const freeTrail = await checkApiLimit();
+    if (!freeTrail) {
+      return new NextResponse("Free trail is expired", { status: 403 });
+    }
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      }
+    );
+    await increaseApiLimit();
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[MUSIC_ERROR]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
