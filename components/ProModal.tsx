bcee553abfeb2b4c7810e0433aba771a3271301c
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-04 19:16:10 +0530

    modified:   components/ProModal.tsx

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -68,6 +68,7 @@ const ProModal = () => {
             variant="premium"
             size="lg"
             onClick={onSubscribe}
+            disabled={loading}
           >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
