1a0e79bfcebc82c02543232c01f5a306fa527881
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-04 18:53:54 +0530

    new file:   app/(dashboard)/(routes)/settings/page.tsx 	modified:   app/(dashboard)/layout.tsx 	modified:   components/FreeCounter.tsx 	modified:   components/Sidebar.tsx 	new file:   components/SubscriptionButton.tsx 	modified:   components/mobile-sidebar.tsx 	modified:   components/navbar.tsx 	new file:   lib/subscription.ts

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,15 @@
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/navbar";
 import { getUserApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const apiLimitCount = await getUserApiLimit();
+  const isPro = await checkSubscription();
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
-        <Sidebar apiLimitCount={apiLimitCount} />
+        <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
       </div>
       <main className="md:pl-72">
         <Navbar />
