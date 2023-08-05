057ef15bb59f50fa5fb8d87ca7ae0f9e76552709
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-05 12:15:04 +0530

    modified:   app/(landing)/page.tsx 	new file:   components/LandingContent.tsx 	new file:   components/LandingHero.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,13 @@
+import { LandingContent } from "@/components/LandingContent";
+import LandingHero from "@/components/LandingHero";
 import LandingNavbar from "@/components/LandingNavbar";
 
 const LandingPage = () => {
   return (
     <div className="h-full">
       <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
